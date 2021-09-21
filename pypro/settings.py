07d5f4faae3797d7292115d1be7b72aa2c71bc2a
Thiago Parente <thiagoparente.ambilev@petrobras.com.br>
2021-09-21 19:53:31 -0300

    Configurado chaves de acesso ao CDN Amazon

--- pypro/settings.py
+++ pypro/settings.py
@@ -117,8 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# Configuração de ambiente de desenvolvimento / coleta dos arquivos estaticos
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configuração para possibilitar upload de arquivos no projeto
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles'
+                          )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
